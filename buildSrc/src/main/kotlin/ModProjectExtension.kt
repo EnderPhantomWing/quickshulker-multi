40e81c530bc986353873d592564dd4b59297aea3
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-06-22 23:44:04 +0800

    chore: mojang -> mojmaps

--- buildSrc/src/main/kotlin/ModProjectExtension.kt
+++ buildSrc/src/main/kotlin/ModProjectExtension.kt
@@ -50,7 +50,7 @@ val Project.fullProjectVersion: String get() = getFullProjectVersion(mcVersion, 
 
 private fun getCommitCountNumber(workDir: File = File(".")): Int? {
     return try {
-        val process = ProcessBuilder("git", "rev-list", "--count", "origin/mojang/preprocessor")
+        val process = ProcessBuilder("git", "rev-list", "--count", "mojmaps/preprocessor")
             .directory(workDir)
             .redirectErrorStream(true)
             .start()
