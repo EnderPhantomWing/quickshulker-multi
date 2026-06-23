b52bf5c98641672a279651b733dbf3675794873f
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-06-24 02:09:51 +0800

    chore: update publish

--- build.unobfuscated.gradle.kts
+++ build.unobfuscated.gradle.kts
@@ -113,14 +113,12 @@ publishing {
     // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
     repositories {
         mavenLocal()
-        if (System.getenv("isPublish") == "true") {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/EnderPhantomWing/quickshulker-multi")
-                credentials {
-                    username = System.getenv("GH_USERNAME")
-                    password = System.getenv("GH_TOKEN")
-                }
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/EnderPhantomWing/quickshulker-multi")
+            credentials {
+                username = System.getenv("GH_USERNAME")
+                password = System.getenv("GH_TOKEN")
             }
         }
     }
