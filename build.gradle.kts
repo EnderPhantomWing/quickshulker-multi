af2bee69cfabf6c4c87152642901d245af2ff53c
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-07-29 02:38:40 +0800

    chore: mirage Gradle Wrapper to Gradle Wrapper Neo
    gradle wrapper neo docs: https://github.com/Glavo/gradle-wrapper-neo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("maven-publish")
     id("com.github.hierynomus.license") version "0.16.1" apply false
-    id("net.fabricmc.fabric-loom") version "1.17.14" apply false
-    id("net.fabricmc.fabric-loom-remap") version "1.17.14" apply false
+    id("net.fabricmc.fabric-loom") version "1.17.17" apply false
+    id("net.fabricmc.fabric-loom-remap") version "1.17.17" apply false
     id("com.replaymod.preprocess") version "c5abb4fb12"
+    id("org.glavo.gradle-wrapper-neo") version "0.2.0"
 }
 
 preprocess {
